f83391f92e46d1ffdecc25f11eb32817c177285d
Nagasai0199 <kodurunagasai02@gmail.com>
2023-08-07 00:36:42 +0530

    all files commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular';
+  "employee":any = [{"id":0,name:"",description:""}];
+  e:any={"id":0,name:"",description:""};
+   url:string="https://localhost:7093/api/Employee"
+  constructor(private _http:HttpClient){
+      this._http.get<any>(this.url).subscribe(x => this.employee = x);
+  }
+  addEmployee(){
+    this._http.post(this.url,this.e,{
+      headers:new HttpHeaders({
+        'Content-Type':'application/json'
+      })
+    }).subscribe(x=>console.log(x));
+  
+  }
 }
